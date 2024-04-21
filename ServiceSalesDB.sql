f49fba89fd96883613f2e31c6f7edf545e61a39f
Гапочка Владислав <72355154+vladgapochka@users.noreply.github.com>
2024-04-21 14:07:40 +0300

    Add files via upload
    БД ServiceSalesDB

---
+++ ServiceSalesDB.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 21 2024 г., 14:06
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `ServiceSalesDB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Clients`
+--
+
+CREATE TABLE `Clients` (
+  `ClientID` int(255) NOT NULL,
+  `Name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Phone` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Products`
+--
+
+CREATE TABLE `Products` (
+  `ProductID` int(255) NOT NULL,
+  `ProductName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CostPerUnit` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `SaleDetails`
+--
+
+CREATE TABLE `SaleDetails` (
+  `SaleDetailID` int(255) NOT NULL,
+  `SaleID` int(255) DEFAULT NULL,
+  `ProductID` int(255) DEFAULT NULL,
+  `Quantity` int(255) NOT NULL,
+  `TotalCost` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Триггеры `SaleDetails`
+--
+DELIMITER $$
+CREATE TRIGGER `CalculateTotalCostBeforeInsert` BEFORE INSERT ON `SaleDetails` FOR EACH ROW BEGIN
+    SET NEW.TotalCost = NEW.Quantity * (SELECT CostPerUnit FROM Products WHERE ProductID = NEW.ProductID);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Sales`
+--
+
+CREATE TABLE `Sales` (
+  `SaleID` int(255) NOT NULL,
+  `ClientID` int(255) DEFAULT NULL,
+  `SaleDateTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Clients`
+--
+ALTER TABLE `Clients`
+  ADD PRIMARY KEY (`ClientID`);
+
+--
+-- Индексы таблицы `Products`
+--
+ALTER TABLE `Products`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Индексы таблицы `SaleDetails`
+--
+ALTER TABLE `SaleDetails`
+  ADD PRIMARY KEY (`SaleDetailID`),
+  ADD KEY `SaleID` (`SaleID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Индексы таблицы `Sales`
+--
+ALTER TABLE `Sales`
+  ADD PRIMARY KEY (`SaleID`),
+  ADD KEY `ClientID` (`ClientID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Clients`
+--
+ALTER TABLE `Clients`
+  MODIFY `ClientID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT для таблицы `Products`
+--
+ALTER TABLE `Products`
+  MODIFY `ProductID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT для таблицы `SaleDetails`
+--
+ALTER TABLE `SaleDetails`
+  MODIFY `SaleDetailID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT для таблицы `Sales`
+--
+ALTER TABLE `Sales`
+  MODIFY `SaleID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `SaleDetails`
+--
+ALTER TABLE `SaleDetails`
+  ADD CONSTRAINT `saledetails_ibfk_1` FOREIGN KEY (`SaleID`) REFERENCES `Sales` (`SaleID`),
+  ADD CONSTRAINT `saledetails_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`);
+
+--
+-- Ограничения внешнего ключа таблицы `Sales`
+--
+ALTER TABLE `Sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`ClientID`) REFERENCES `Clients` (`ClientID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
